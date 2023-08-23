f95e29a4067608ef7959366d545888e8f41f65c7
Laura Andrade <lauraandradedeoliveira@gmail.com>
2023-08-23 08:43:34 -0300

    atualização

---
+++ teste/lista2.js
@@ -0,0 +1,19 @@
+function exe5(){
+    let escolha = Number(document.getElementById("escolha").value);
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    
+    switch (escolha) {
+        case 1: 
+        let media = (num1 + num2)/2
+        document.getElementById("resultado").innerHTML=`Teste`
+        break
+        case 2:
+            break
+        case 3:
+            break
+        case 4:
+            break
+    }
+    
+}
