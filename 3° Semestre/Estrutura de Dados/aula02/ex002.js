921104b20f2bb95366f0d0ed37502767f786218c
Laura Andrade <lauraandradedeoliveira@gmail.com>
2024-02-22 10:58:22 -0300

    updates

--- 3° Semestre/Estrutura de Dados/aula02/ex002.js
+++ 3° Semestre/Estrutura de Dados/aula02/ex002.js
@@ -1,6 +1,6 @@
 // método bubblesort
 function bubblesort(){
-    var vetor = [7,3,7,8,1]
+    var vetor = [10,20,12,5,8]
 
     for(var i = 0;i<vetor.length;i++){
         for(var j = 0;j<vetor.length;j++){
---
+++ 3° Semestre/Estrutura de Dados/aula03/ex003.js
@@ -0,0 +1,19 @@
+// Seleção Direta
+function selectionSort() { // criando a função
+  var vetor = [25, 57, 48, 37, 12, 92, 86, 33]; // definindo os valores do vetor
+
+  for (var i = 0; i < vetor.length; i++) { // loop externo que percorre o vetor
+    var menor = i; // definindo o menor valor como o primeiro elemento do vetor. Ele será usado para armazenar o índice do menor elemento
+
+    for (var j = i + 1; j < vetor.length; j++) { // loop interno que percorre o vetor
+      if (vetor[j] < vetor[menor]) { //Compara o elemento atual (vetor[j]) com o menor elemento encontrado até agora (vetor[menor])
+        menor = j; // se o elemento atual for menor que o menor elemento encontrado até agora, atualiza o índice menor
+      }
+    }
+
+    if (menor !== i) { // se menor for diferente de i, isso indica que precisamos trocar os elementos de posição para garantir que o elemento menor esteja na posição correta
+      [vetor[i], vetor[menor]] = [vetor[menor], vetor[i]]; // troca os elementos de posição
+    }
+  }
+  console.log(vetor); // exibe o vetor ordenado
+}
