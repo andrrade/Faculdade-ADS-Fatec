6a254cd49af86e10784758a3a001eab27f58c4bf
Laura Andrade <devandrrade@gmail.com>
2024-08-15 09:53:47 -0300

    realizando o exercicio02-Cliente

--- 4-semestre/programacao-orientada-a-objetos/listas/lista01/src/main/java/br/edu/fatecfranca/exercicio02/Cliente.java
+++ 4-semestre/programacao-orientada-a-objetos/listas/lista01/src/main/java/br/edu/fatecfranca/exercicio02/Cliente.java
@@ -1,4 +1,74 @@
 package br.edu.fatecfranca.exercicio02;
 
 public class Cliente {
+    public int numeroConta;
+    public int numeroAgencia;
+    public String nome;
+    public double saldo;
+
+    public Cliente(int numeroConta, int numeroAgencia, String nome, double saldo) {
+        this.numeroConta = numeroConta;
+        this.numeroAgencia = numeroAgencia;
+        this.nome = nome;
+        this.saldo = saldo;
+
+        if(numeroConta <= 0){
+            System.out.println("Número da conta INVÁLIDO!!!");
+        }
+
+        if(numeroAgencia <= 0){
+            System.out.println("Número da agência INVÁLIDO!!!");
+        }
+
+        if(saldo < 0){
+            System.out.println("Valor de saldo INVÁLIDO!!!");
+        }
+        else{
+            System.out.println("Saldo inicial: " + this.saldo);
+        }
+    }
+
+    public Cliente(){
+        this.nome = " ";
+    }
+
+    public void realizarDeposito(int x){
+        if(saldo >= 0){
+            if(x > 0){
+                this.saldo += x;
+                System.out.println("Valor do depósito: " + x);
+            }
+            else{
+                System.out.println("Valor INVÁLIDO para realizar depósito!!!");
+            }
+        }
+    }
+
+    public void realizarSaque(int x){
+        if(saldo >= 0){
+            if(x > 0 && x <= this.saldo){
+                this.saldo -= x;
+                System.out.println("Valor do saque: " + x);
+            }
+            else{
+                System.out.println("Valor INVÁLIDO para realizar saque!!!");
+            }
+        }
+    }
+
+    public void dadosCliente(){
+        if(numeroConta > 0){
+            System.out.println("Número da conta: " + this.numeroConta);
+        }
+
+        if(numeroAgencia > 0){
+            System.out.println("Número da agência: " + this.numeroAgencia);
+        }
+
+        if(saldo >= 0) {
+            System.out.println("Saldo atual: " + this.saldo);
+        }
+
+        System.out.println("Nome do cliente: " + this.nome + "\n");
+     }
 }
--- 4-semestre/programacao-orientada-a-objetos/listas/lista01/src/main/java/br/edu/fatecfranca/exercicio02/TestaCliente.java
+++ 4-semestre/programacao-orientada-a-objetos/listas/lista01/src/main/java/br/edu/fatecfranca/exercicio02/TestaCliente.java
@@ -1,4 +1,16 @@
 package br.edu.fatecfranca.exercicio02;
 
 public class TestaCliente {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente(123, 87,"Fulano",1000);
+
+        cliente1.realizarDeposito(500);
+        cliente1.realizarSaque(300);
+        cliente1.dadosCliente();
+
+        Cliente cliente2 = new Cliente(-321,-75, "Beltrano", -9);
+        cliente2.realizarDeposito(700);
+        cliente2.realizarSaque(250);
+        cliente2.dadosCliente();
+    }
 }
