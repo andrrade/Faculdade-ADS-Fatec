6b61b69e40005202a36ffae6ff617ca655c7a1cb
Laura Andrade <devandrrade@gmail.com>
2024-08-15 06:45:12 -0300

    realizando o exercicio dos carros

--- 4-semestre/programacao-orientada-a-objetos/listas/lista01/src/main/java/br/edu/fatecfranca/exercicio00/Carro.java
+++ 4-semestre/programacao-orientada-a-objetos/listas/lista01/src/main/java/br/edu/fatecfranca/exercicio00/Carro.java
@@ -1,4 +1,28 @@
 package br.edu.fatecfranca.exercicio00;
 
 public class Carro {
+    // variáveis
+    public String marca, modelo, cor;
+    public int ano;
+
+    // construtor
+    public Carro(String marca, String modelo, String cor, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ano = ano;
+    }
+
+    // métodos
+    public void acelerar(){
+        System.out.println(this.modelo + " está acelerando! VRUMM");
+    }
+
+    public void frear(){
+        System.out.println(this.modelo + " freou...");
+    }
+    
+    public String toString(){
+        return this.marca + "\n" + this.modelo + "\n" + this.cor + "\n" + this.ano + "\n";
+    }
 }
